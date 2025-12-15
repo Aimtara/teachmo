7aa5166770f8587d86dea1d72c2ada3ecc98d2c9
Aimtara <223397310+Aimtara@users.noreply.github.com>
2025-12-15 17:06:30 -0500

    Add safe HTML sanitizer and CSP helper

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useAuthenticationStatus } from '@nhost/react';
