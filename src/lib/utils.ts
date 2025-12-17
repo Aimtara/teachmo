48f5c94320892de4708dd90fd837077118d07319
Aimtara <223397310+Aimtara@users.noreply.github.com>
2025-12-16 22:06:49 -0500

    Restore cn helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]): string {
+export function cn(...inputs) {
   return twMerge(clsx(inputs));
-}
-
-export function formatDate(value: string | number | Date, locale = 'en-US', options?: Intl.DateTimeFormatOptions): string {
-  const date = value instanceof Date ? value : new Date(value);
-  if (Number.isNaN(date.getTime())) return '';
-
-  return new Intl.DateTimeFormat(locale, options ?? { dateStyle: 'medium' }).format(date);
 }
