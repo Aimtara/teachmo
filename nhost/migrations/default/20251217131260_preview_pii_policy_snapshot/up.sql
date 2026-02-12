56fea5b0cb7170fc96dc5885e41880f663ef8d4d
aimtara <you@example.com>
2026-02-12 17:41:27 -0500

    Fix: Remove duplicate migration timestamps

--- nhost/migrations/default/20251217131260_preview_pii_policy_snapshot/up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.directory_import_previews
-  ADD COLUMN IF NOT EXISTS pii_policy_snapshot jsonb NOT NULL DEFAULT '{}'::jsonb;
--- nhost/migrations/default/20251217131261_quarantine_redacted/down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.directory_import_quarantine
-  DROP COLUMN IF EXISTS raw_redacted;
