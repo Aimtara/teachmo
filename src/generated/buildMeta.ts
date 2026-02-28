02e2fe440ad41750f7ce8414ecb31c818afb4427
aimtara <you@example.com>
2026-02-28 14:08:07 -0500

    Fix: Nuke archived onboarding file to force correct routing

--- src/generated/buildMeta.ts
+++ src/generated/buildMeta.ts
@@ -1,3 +1,3 @@
-export const BUILD_SHA = "45ffed394af7a0a7d6062e3fed36b8449123b2e2";
-export const BUILD_TIME = "2026-02-10T02:41:10.438Z";
+export const BUILD_SHA = "e3990d2b9a780210a94f1fbdea0b87bf8ba838d5";
+export const BUILD_TIME = "2026-02-26T21:54:10.753Z";
 export const APP_ENV = "development";
